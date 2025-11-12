bf414cb094f8ab8bea61939af04927c92639b03d
khushbu <pujakumari29122001@gmail.com>
2025-11-12 22:16:50 +0530

    Add comments to store.js to explain Redux store setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./booksSlice";
 
+// Configure the Redux store
 const store = configureStore({
   reducer: {
+    // Add the books slice reducer
     books: booksReducer,
   },
 });
 
+// Export the store to be used in the app
 export default store;
