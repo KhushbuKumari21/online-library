0054cc613cbcbf6e5182012ef82dcd4245f26e53
khushbu <pujakumari29122001@gmail.com>
2025-11-12 22:17:54 +0530

    Add comments to Navbar.jsx for better readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
+    // Main navigation bar
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      {/* Brand / Logo */}
       <Link className="navbar-brand" to="/">📚 Online Library</Link>
+
+      {/* Navigation links */}
       <div>
         <Link className="nav-link d-inline text-light px-2" to="/">Home</Link>
         <Link className="nav-link d-inline text-light px-2" to="/books/All">Browse Books</Link>
